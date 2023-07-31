5daf142f34567b2217b5d98298c6e3e1b939dd88
qm <129896565+klisarov@users.noreply.github.com>
2023-07-31 03:05:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
   event.preventDefault(); 
 
-  // zemanje na podatoci
+  // vnes na user i psw
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
+  // proverka na podatocite, tocen user e admin i tocen psw e password
   if (username === "admin" && password === "password") {
-    // ako loginot e uspesen, prenasocuva na index fileot
+    // ako informaciite se tocni, nosi na https://www.klisarov.me
     window.location.href = "https://www.klisarov.me";
   } else {
-    // ako e pogresen username ili psw, se dava alert
-    alert("Invalid username or password. Please try again.");
+    // vo slucaj da ne e tocen login, dava alert
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Invalid username or password. Please try again.";
+    errorMessage.classList.add("error-message");
+    document.getElementById("loginForm").appendChild(errorMessage);
   }
 });
